e73b73925f125ba6633f2357779af8350176c453
Matheus <Matheus@DEV>
2024-07-08 12:37:47 +0200

    Add project files.

---
+++ CLIENTS_MANAGEMENT_SYSTEM.Module/BusinessObjects/Client.cs
@@ -0,0 +1,79 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+
+namespace CLIENTS_MANAGEMENT_SYSTEM.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    [NavigationItem("Clients")]
+    public class Client : BaseObject
+    {
+        public Client(Session session) : base(session) { }
+
+        string name;
+        string clientCode;
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Name
+        {
+            get => name;
+            set => SetPropertyValue(nameof(Name), ref name, value);
+        }
+
+        [Persistent("ClientCode")]
+        [Size(6)]
+        public string ClientCode
+        {
+            get => clientCode;
+            protected set => SetPropertyValue(nameof(ClientCode), ref clientCode, value);
+        }
+
+        protected override void OnSaving()
+        {
+            if (string.IsNullOrEmpty(ClientCode))
+            {
+                ClientCode = GenerateClientCode();
+            }
+            base.OnSaving();
+        }
+
+        private string GenerateClientCode()
+        {
+            var alphaPart = new string(Name.ToUpper().Take(3).ToArray());
+            if (alphaPart.Length < 3)
+            {
+                alphaPart = alphaPart.PadRight(3, 'A');
+            }
+
+            var clientCodes = Session.Query<Client>()
+                                     .Where(c => c.ClientCode.StartsWith(alphaPart))
+                                     .Select(c => c.ClientCode)
+                                     .ToList();
+
+            int maxNumber = 0;
+            if (clientCodes.Any())
+            {
+                maxNumber = clientCodes
+                            .Select(code => int.Parse(code.Substring(3)))
+                            .Max();
+            }
+
+            var numericPart = (maxNumber + 1).ToString("D3");
+            return alphaPart + numericPart;
+        }
+
+        [PersistentAlias("Contacts.Count()")]
+        public int LinkedContactsCount
+        {
+            get
+            {
+                object tempObject = EvaluateAlias(nameof(LinkedContactsCount));
+                return tempObject != null ? (int)tempObject : 0;
+            }
+        }
+
+        [Association("Client-Contacts")]
+        public XPCollection<Contact> Contacts => GetCollection<Contact>(nameof(Contacts));
+    }
+
+}
---
+++ CLIENTS_MANAGEMENT_SYSTEM.Module/BusinessObjects/Contact.cs
@@ -0,0 +1,44 @@
+﻿
+using CLIENTS_MANAGEMENT_SYSTEM.Module.BusinessObjects;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+
+[DefaultClassOptions]
+[NavigationItem("Clients")]
+public class Contact : BaseObject
+{
+    public Contact(Session session) : base(session) { }
+
+    string name;
+    string surname;
+    string email;
+
+    [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+    public string Name
+    {
+        get => name;
+        set => SetPropertyValue(nameof(Name), ref name, value);
+    }
+
+    [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+    public string Surname
+    {
+        get => surname;
+        set => SetPropertyValue(nameof(Surname), ref surname, value);
+    }
+
+    [RuleRequiredField(DefaultContexts.Save)]
+    [RuleRegularExpression(DefaultContexts.Save, "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", CustomMessageTemplate = "Invalid email format.")]
+    [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+    public string Email
+    {
+        get => email;
+        set => SetPropertyValue(nameof(Email), ref email, value);
+    
+    }
+
+        [Association("Client-Contacts")]
+    public XPCollection<Client> Clients => GetCollection<Client>(nameof(Clients));
+}
